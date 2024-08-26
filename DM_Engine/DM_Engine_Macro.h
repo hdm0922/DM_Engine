47d9f032d984572fa55f9b3edaca5000cc12c771
Dongmin Han <hdm0922@gmail.com>
2024-08-26 14:41:06 +0900

    사용자 정의 매크로 통합

---
+++ DM_Engine/DM_Engine_Macro.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define NULL 0
+#define DM_RELATIVE_PATH_FROM_ENGINE_TO_RESOURCES				L"..//Resources//"
+
+#define DM_TEST_TEXTURE_BMP_NAME								L"Test.bmp"
+#define DM_TEST_TEXTURE_BMP_PATH								L"..//Resources//AlphaCat.bmp"
+
+#define DM_TEST_TEXTURE_PNG_NAME								L"Test.png"
+#define DM_TEST_TEXTURE_PNG_PATH								L"..//Resources//Haley.png"
+
+#define DM_TEST_ANIMATION_NAME									L"TESTANIMATION"
+#define DM_TEST_SCENE_NAME										L"TestScene"
--- DM_Engine/framework.h
+++ DM_Engine/framework.h
@@ -41,17 +41,6 @@
 
 
 // 사용자 정의
+#include "DM_Engine_Macro.h"
 #include "DM_Math.h"
 #include "DM_Enums.h"
-
-#define NULL 0
-#define DM_RELATIVE_PATH_FROM_ENGINE_TO_RESOURCES L"..//Resources//"
-
-#define DM_TEST_TEXTURE_BMP_NAME L"Test.bmp"
-#define DM_TEST_TEXTURE_BMP_PATH L"..//Resources//AlphaCat.bmp"
-
-#define DM_TEST_TEXTURE_PNG_NAME L"Test.png"
-#define DM_TEST_TEXTURE_PNG_PATH L"..//Resources//Haley.png"
-
-#define DM_TEST_ANIMATION_NAME L"TESTANIMATION"
-#define DM_TEST_SCENE_NAME L"TestScene"
